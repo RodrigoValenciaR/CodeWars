95b68b036c4a1ea7c1b60fff36a563919f6727e0
Valencia Rojas <r.valencia.rojas@accenture.com>
2020-10-19 10:37:43 +0200

    First Kata of code wars and his Junit test

---
+++ Code Wars/Kata1.java
@@ -0,0 +1,12 @@
+package codeWars1;
+
+public class Kata1 {
+	   public static int summation(int n) {
+		   	int resul=0,cont=0;
+		   	while(cont<n) {
+		   		cont++;
+		   		resul=resul+cont;
+		   	}
+	        return resul;
+	    }
+}
