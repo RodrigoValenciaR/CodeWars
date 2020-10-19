645470d984a87fbb81ea4007225b273c20734e18
Valencia Rojas <r.valencia.rojas@accenture.com>
2020-10-19 11:18:00 +0200

    Katas added from 1 to 19 and every Junit test

--- Code Wars/Kata4.java
+++
@@ -1,7 +0,0 @@
-package codeWars1;
-
-public class Kata4 {
-	  public int cockroachSpeed(double x){
-		  return (int)(x*100000/3600);
-		  }
-}
--- Code Wars/Kata8.java
+++
@@ -1,8 +0,0 @@
-package codeWars1;
-
-public class Kata8 {
-	  public int Liters(double time)  {
-		    return (int)((int)time*0.5);
-		  
-		  }
-}
